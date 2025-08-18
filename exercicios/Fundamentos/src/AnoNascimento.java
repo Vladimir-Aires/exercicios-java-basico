adfebbaea576ebb460750f63be2a570d145169f8
Vladimir-Aires <vlad.aires05@gmail.com>
2025-08-18 16:35:04 -0300

    Exercicio de fundamentos 1 - Ano de Nascimento

--- exercicios/Fundamentos/src/AnoNascimento.java
+++ exercicios/Fundamentos/src/AnoNascimento.java
@@ -1,5 +1,20 @@
+import java.time.OffsetDateTime;
+import java.util.Scanner;
+
 public class AnoNascimento {
     public static void main(String[] args){
-        
+        var scanner = new Scanner(System.in);
+
+        var anoBase = OffsetDateTime.now().getYear();
+
+        System.out.print("Seja bem vindo(a)! \nInforme-nos o seu nome: ");
+        var nome = scanner.next();
+
+        System.out.print("Agora, nos informe o seu ano de nascimento: ");
+        var anoNasc = scanner.nextInt();
+
+        var idade = anoBase - anoNasc;
+
+        System.out.printf("Muito prazer %s! Você tem ou vai fazer %s anos.", nome, idade);
     }
 }
